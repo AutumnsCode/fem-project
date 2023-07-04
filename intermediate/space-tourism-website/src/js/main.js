3a1e797437824613a87085f4055384260f6a4eda
AutumnCod <109141163+AutumnsCode@users.noreply.github.com>
2023-07-04 21:51:01 +0200

    new changes

--- intermediate/space-tourism-website/src/js/main.js
+++ intermediate/space-tourism-website/src/js/main.js
@@ -1,1 +1,17 @@
 import '../scss/main.scss';
+
+const btn = document.querySelector(".primary-header__menu");
+const nav = document.querySelector(".primary-navigation");
+
+
+btn.addEventListener("click", () => {
+  const visibilty = nav.getAttribute('data-visible');
+
+  if( visibilty == "false") {
+    nav.setAttribute('data-visible', true);
+    btn.setAttribute('aria-expended', true);
+  } else {
+    nav.setAttribute('data-visible', false);
+    btn.setAttribute('aria-expended', false);
+  }
+})
